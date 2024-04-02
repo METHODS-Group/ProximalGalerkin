5dd92c2cd804c52f6703b774dbe4f028c224d00b
Jørgen S. Dokken <dokken@simula.no>
2024-04-02 16:42:12 +0200

    Refactir gradient constraint for proper control

--- examples/gradient_constraint/script.py
+++ examples/gradient_constraint/script.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import numpy as np
 from mpi4py import MPI
 from petsc4py import PETSc
@@ -6,157 +7,238 @@ import basix.ufl
 import ufl
 from dolfinx.nls.petsc import NewtonSolver
 from dolfinx.fem.petsc import NonlinearProblem, LinearProblem
+from typing import Optional
+import argparse
+from enum import Enum
+from typing import Callable
 
-N = 80
-M = 77
-mesh = dolfinx.mesh.create_unit_square(
-    MPI.COMM_WORLD, N, M)
-# , dolfinx.mesh.CellType.quadrilateral)
+class AlphaScheme(Enum):
+    constant = 1 # Constant alpha (alpha_0)
+    linear = 2 # Linearly increasing alpha (alpha_0 + alpha_c * i) where i is the iteration number
+    doubling = 3 # Doubling alpha (alpha_0 * 2^i) where i is the iteration number
 
-el_0 = basix.ufl.element("Lagrange", mesh.topology.cell_name(), 2)
-el_1 = basix.ufl.element(
-    "DG", mesh.topology.cell_name(), 1, shape=(mesh.geometry.dim,))
-# el_1 = basix.ufl.element(
-#     "RT", mesh.topology.cell_name(), 1)
-trial_el = basix.ufl.mixed_element([el_0, el_1])
-V_trial = dolfinx.fem.functionspace(mesh, trial_el)
-# test_el = basix.ufl.mixed_element([el_0, el_1])
-# V_test = dolfinx.fem.functionspace(mesh, test_el)
-V_test = V_trial
+    @classmethod
+    def from_string(cls, method:str):
+        if method == "constant":
+            return AlphaScheme.constant
+        elif method == "linear":
+            return AlphaScheme.linear
+        elif method == "doubling":
+            return AlphaScheme.doubling
+        else:
+            raise ValueError(f"Unknown alpha scheme {method}")
 
-w = dolfinx.fem.Function(V_trial)
-u, psi = ufl.split(w)
+def solve_problem(N:int, M:int,
+                  primal_space: str,
+                  dual_space: str,
+                  primal_degree: int,
+                  cell_type: str,
+                  alpha_scheme: AlphaScheme,
+                  alpha_0: float,
+                  alpha_c: float,
+                  max_iterations: int,
+                  result_dir: Path,
+                  phi_func: Callable[[np.ndarray], np.ndarray],
+                  f_func: Callable[[np.ndarray], np.ndarray],
+                  warm_start:bool):
+    _cell_type = dolfinx.mesh.to_type(cell_type)
 
-v, tau = ufl.TestFunctions(V_test)
+    mesh = dolfinx.mesh.create_unit_square(
+        MPI.COMM_WORLD, N, M, cell_type=_cell_type)
 
-dx = ufl.Measure("dx",  domain=mesh)
-alpha = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type(1))
-x = ufl.SpatialCoordinate(mesh)
-phi = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type(1))
-# phi = 1 + x[0]+2*x[1]
-w0 = dolfinx.fem.Function(V_trial)
-u0, psi0 = ufl.split(w0)
+    el_0 = basix.ufl.element(primal_space, mesh.topology.cell_name(), primal_degree)
+    if dual_space == "RT":
+        el_1 = basix.ufl.element(
+            "RT", mesh.topology.cell_name(), primal_degree -1)
+    elif dual_space == "DG":
+        el_1 = basix.ufl.element(
+            "DG", mesh.topology.cell_name(), primal_degree-1, shape=(mesh.geometry.dim,))
 
-amp = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type(1))
-f = amp*10.
-# f = amp*ufl.sin(2*x[0]*ufl.pi)*ufl.sin(x[1]*ufl.pi)
-# f = amp*(x[0]*x[1]*(x[0]-1.)*(x[1]-1.))**2
+    trial_el = basix.ufl.mixed_element([el_0, el_1])
+    V_trial = dolfinx.fem.functionspace(mesh, trial_el)
+    V_test = V_trial
 
-mesh.topology.create_connectivity(mesh.topology.dim-1, mesh.topology.dim)
-boundary_facets = dolfinx.mesh.exterior_facet_indices(mesh.topology)
+    sol = dolfinx.fem.Function(V_trial)
+    u, psi = ufl.split(sol)
 
-U, U_to_W = V_trial.sub(0).collapse()
-Q, Q_to_W = V_trial.sub(1).collapse()
-boundary_dofs = dolfinx.fem.locate_dofs_topological(
-    (V_trial.sub(0), U), mesh.topology.dim-1, boundary_facets)
+    v, w = ufl.TestFunctions(V_test)
 
-# Create initial condition
-p = ufl.TrialFunction(U)
-q = ufl.TestFunction(U)
-a = ufl.inner(ufl.grad(p), ufl.grad(q))*dx
-L = f * q * dx
-u_bc_init = dolfinx.fem.Function(U)
-bc_init = dolfinx.fem.dirichletbc(u_bc_init, boundary_dofs[1])
-lin_prob = LinearProblem(a, L, bcs=[bc_init])
-u_init_out = lin_prob.solve()
-u_init_out.name = "InitialU"
-# u init is equal to the solution of the linear problem
-w.x.array[U_to_W] = u_init_out.x.array
-w.x.array[Q_to_W] = 0.
+    dx = ufl.Measure("dx",  domain=mesh)
+    alpha = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type(alpha_0))
+    U, U_to_W = V_trial.sub(0).collapse()
+    phi = dolfinx.fem.Function(U)
+    phi.interpolate(phi_func)
 
+    w0 = dolfinx.fem.Function(V_trial)
 
-F = alpha * ufl.inner(ufl.grad(u), ufl.grad(v))*dx
-F -= ufl.inner(ufl.div(psi), v)*dx
-F -= alpha * ufl.inner(f, v) * dx
-F += ufl.inner(ufl.div(psi0), v)*dx
-F += ufl.inner(ufl.grad(u), tau)*dx
-non_lin_term = 1/(ufl.sqrt(1 + ufl.dot(psi, psi)))
-F -= phi * non_lin_term * ufl.dot(psi, tau)*dx
+    f = dolfinx.fem.Function(U)
+    f.interpolate(f_func)
 
+    mesh.topology.create_connectivity(mesh.topology.dim-1, mesh.topology.dim)
+    boundary_facets = dolfinx.mesh.exterior_facet_indices(mesh.topology)
 
-u_bc = dolfinx.fem.Function(U)
-u_bc.x.array[:] = 0
-bcs = [dolfinx.fem.dirichletbc(u_bc, boundary_dofs, V_trial.sub(0))]
+    Q, Q_to_W = V_trial.sub(1).collapse()
+    boundary_dofs = dolfinx.fem.locate_dofs_topological(
+        (V_trial.sub(0), U), mesh.topology.dim-1, boundary_facets)
 
+    if warm_start:
+    
+        # Create initial condition
+        p = ufl.TrialFunction(U)
+        q = ufl.TestFunction(U)
+        a = ufl.inner(ufl.grad(p), ufl.grad(q))*dx
+        L = f * q * dx
+        u_bc_init = dolfinx.fem.Function(U)
+        bc_init = dolfinx.fem.dirichletbc(u_bc_init, boundary_dofs[1])
+        lin_prob = LinearProblem(a, L, bcs=[bc_init])
+        u_init_out = lin_prob.solve()
+        u_init_out.name = "InitialU"
+        # u init is equal to the solution of the linear problem
+        sol.x.array[U_to_W] = u_init_out.x.array
+        sol.x.array[Q_to_W] = 0.
 
-problem = NonlinearProblem(F, w, bcs=bcs)
-solver = NewtonSolver(mesh.comm, problem)
-solver.convergence_criterion = "residual"
-solver.rtol = 1e-9
-solver.atol = 1e-9
-solver.max_it = 20
-# solver.relaxation_factor = 0.7
-solver.error_on_nonconvergence = False
+    _, psi0 = ufl.split(w0)
 
-
-ksp = solver.krylov_solver
-opts = PETSc.Options()  # type: ignore
-option_prefix = ksp.getOptionsPrefix()
-opts[f"{option_prefix}ksp_type"] = "preonly"
-opts[f"{option_prefix}pc_type"] = "lu"
-opts[f"{option_prefix}pc_factor_mat_solver_type"] = "superlu"
-
-# For factorisation prefer MUMPS, then superlu_dist, then default.
-# sys = PETSc.Sys()  # type: ignore
-# if sys.hasExternalPackage("mumps"):
-#     opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
-# elif sys.hasExternalPackage("superlu_dist"):
-#     opts[f"{option_prefix}pc_factor_mat_solver_type"] = "superlu_dist"
-ksp.setFromOptions()
-
-
-# T = dolfinx.fem.functionspace(mesh, ("DG", 0, (mesh.geometry.dim,)))
-# grad_psi = dolfinx.fem.Expression(
-#     ufl.grad(w.sub(1)), T.element.interpolation_points())
-# t = dolfinx.fem.Function(T)
-# t.name = "grad_psi"
-
-Q = dolfinx.fem.functionspace(mesh, ("DG", 1, (mesh.geometry.dim, )))
-grad_u = dolfinx.fem.Expression(
-    ufl.grad(w.sub(0)), Q.element.interpolation_points())
-q_out = dolfinx.fem.Function(Q)
-q_out.name = "gradu"
+    F = alpha * ufl.inner(ufl.grad(u), ufl.grad(v))*dx
+    F += ufl.inner(psi, ufl.grad(v))*dx
+    F -= alpha * ufl.inner(f, v) * dx
+    F -= ufl.inner(psi0, ufl.grad(v))*dx
+    
+    F += ufl.inner(ufl.grad(u), w)*dx
+    non_lin_term = 1/(ufl.sqrt(1 + ufl.dot(psi, psi)))
+    F -= phi * non_lin_term * ufl.dot(psi, w)*dx
 
 
-dolfinx.log.set_log_level(dolfinx.log.LogLevel.INFO)
-# xdmf = dolfinx.io.XDMFFile(MPI.COMM_WORLD, "output.xdmf", "w")
-# xdmf.write_mesh(mesh)
-u_out = w.sub(0).collapse()
-u_out.name = "u"
-bp_u = dolfinx.io.VTXWriter(mesh.comm, "u.bp", [u_out], engine="BP4")
-
-bp_grad_u = dolfinx.io.VTXWriter(mesh.comm, "grad_u.bp", [q_out], engine="bp4")
+    u_bc = dolfinx.fem.Function(U)
+    u_bc.x.array[:] = 0
+    bcs = [dolfinx.fem.dirichletbc(u_bc, boundary_dofs, V_trial.sub(0))]
 
 
-diff = w.sub(0)-w0.sub(0)
-L2_squared = ufl.dot(diff, diff)*dx
-compiled_diff = dolfinx.fem.form(L2_squared)
+    problem = NonlinearProblem(F, sol, bcs=bcs)
+    solver = NewtonSolver(mesh.comm, problem)
+    solver.convergence_criterion = "residual"
+    solver.rtol = 1e-9
+    solver.atol = 1e-9
+    solver.max_it = 20
+    solver.error_on_nonconvergence = True
 
-for i in range(13):
-    alpha.value = 2**i
-    num_newton_iterations, converged = solver.solve(w)
-    # ksp.view()
-    print(
-        f"Iteration {i}: {converged=} {num_newton_iterations=} {ksp.getConvergedReason()=}")
-    local_diff = dolfinx.fem.assemble_scalar(compiled_diff)
-    global_diff = np.sqrt(mesh.comm.allreduce(local_diff, op=MPI.SUM))
-    print(f"|delta u |= {global_diff}")
-    w0.x.array[:] = w.x.array
 
-    dolfinx.log.set_log_level(dolfinx.log.LogLevel.ERROR)
+    ksp = solver.krylov_solver
+    opts = PETSc.Options()  # type: ignore
+    option_prefix = ksp.getOptionsPrefix()
+    opts[f"{option_prefix}ksp_type"] = "preonly"
+    opts[f"{option_prefix}pc_type"] = "lu"
+    opts[f"{option_prefix}pc_factor_mat_solver_type"] = "superlu"
+    ksp.setFromOptions()
 
-    u_out.x.array[:] = w.sub(0).x.array[U_to_W]
-    bp_u.write(i)
-    q_out.interpolate(grad_u)
 
-    bp_grad_u.write(i)
+    # T = dolfinx.fem.functionspace(mesh, ("DG", 0, (mesh.geometry.dim,)))
+    # grad_psi = dolfinx.fem.Expression(
+    #     ufl.grad(sol.sub(1)), T.element.interpolation_points())
+    # t = dolfinx.fem.Function(T)
+    # t.name = "grad_psi"
 
-    # t.interpolate(grad_psi)
+    # Q = dolfinx.fem.functionspace(mesh, ("DG", 1, (mesh.geometry.dim, )))
+    # grad_u = dolfinx.fem.Expression(
+    #     ufl.grad(sol.sub(0)), Q.element.interpolation_points())
+    # q_out = dolfinx.fem.Function(Q)
+    # q_out.name = "gradu"
 
-    # xdmf.write_function(u_out, i)
-    # xdmf.write_function(psi_out, i)
-    # xdmf.write_function(q_out, i)
-    # xdmf.write_function(t, i)
-# xdmf.close()
-bp_u.close()
-bp_grad_u.close()
+
+    dolfinx.log.set_log_level(dolfinx.log.LogLevel.INFO)
+
+    u_out = sol.sub(0).collapse()
+    u_out.name = "u"
+    bp_u = dolfinx.io.VTXWriter(mesh.comm, result_dir / "u.bp", [u_out], engine="BP4")
+
+    # bp_grad_u = dolfinx.io.VTXWriter(mesh.comm, "grad_u.bp", [q_out], engine="bp4")
+
+
+    diff = sol.sub(0)-w0.sub(0)
+    L2_squared = ufl.dot(diff, diff)*dx
+    compiled_diff = dolfinx.fem.form(L2_squared)
+
+    for i in range(max_iterations):
+        if alpha_scheme == AlphaScheme.constant:
+            pass
+        elif alpha_scheme == AlphaScheme.linear:
+            alpha.value = alpha_0 + alpha_c * i
+        elif alpha_scheme == AlphaScheme.doubling:
+            alpha.value = alpha_0 * 2**i
+
+        num_newton_iterations, converged = solver.solve(sol)
+
+        print(
+            f"Iteration {i}: {converged=} {num_newton_iterations=} {ksp.getConvergedReason()=}")
+        local_diff = dolfinx.fem.assemble_scalar(compiled_diff)
+        global_diff = np.sqrt(mesh.comm.allreduce(local_diff, op=MPI.SUM))
+        print(f"|delta u |= {global_diff}")
+        w0.x.array[:] = sol.x.array
+
+        dolfinx.log.set_log_level(dolfinx.log.LogLevel.ERROR)
+
+        u_out.x.array[:] = sol.sub(0).x.array[U_to_W]
+        bp_u.write(i)
+        # q_out.interpolate(grad_u)
+
+        # bp_grad_u.write(i)
+
+        # t.interpolate(grad_psi)
+
+        # xdmf.write_function(u_out, i)
+        # xdmf.write_function(psi_out, i)
+        # xdmf.write_function(q_out, i)
+        # xdmf.write_function(t, i)
+    # xdmf.close()
+    bp_u.close()
+    # bp_grad_u.close()
+
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawTextHelpFormatter):
+    pass
+
+def main(argv: Optional[list[str]]=None):
+    parser = argparse.ArgumentParser(formatter_class=CustomFormatter)
+    mesh_options = parser.add_argument_group("Mesh options")
+    mesh_options.add_argument("-N", type=int, default=10, help="Number of elements in x-direction")
+    mesh_options.add_argument("-M", type=int, default=10, help="Number of elements in y-direction")
+    mesh_options.add_argument("--cell_type", "-c", type=str, default="triangle", choices=["triangle", "quadrilateral"], help="Cell type")
+    element_options = parser.add_argument_group("Finite element discretization options")
+    element_options.add_argument("--primal_space", type=str, default="Lagrange", choices=["Lagrange", "P", "CG"], help="Finite Element family for primal variable")
+    element_options.add_argument("--dual_space", type=str, default="RT", choices=["RT", "DG"], help="Finite element family for auxiliary variable")
+    element_options.add_argument("--primal_degree", type=int, default=2, choices=[2,3,4,5,6,7,8], help="Polynomial degree for primal variable")
+    alpha_options = parser.add_argument_group("Options for alpha-variable in Proximal Galerkin scheme")
+    alpha_options.add_argument("--alpha_scheme", type=str, default="linear", choices=["constant", "linear", "doubling"], help="Scheme for updating alpha")
+    alpha_options.add_argument("--alpha_0", type=float, default=1.0, help="Initial value of alpha")
+    alpha_options.add_argument("--alpha_c", type=float, default=1.0, help="Increment of alpha in linear scheme")
+    pg_options = parser.add_argument_group("Proximal Galerkin options")
+    pg_options.add_argument("--max_iterations", type=int, default=10, help="Maximum number of iterations")
+    pg_options.add_argument("--warm_start", action="store_true", help="Use warm start (solve Poisson problem to get initial guess)")
+    result_options = parser.add_argument_group("Output options")
+    result_options.add_argument("--result_dir", type=Path, default=Path("results"), help="Directory to store results")
+    parsed_args = parser.parse_args(argv)
+
+    def phi_func(x):
+        return np.ones(x.shape[1])
+    def f_func(x):
+        return np.ones(x.shape[1])
+
+    solve_problem(N=parsed_args.N, M=parsed_args.M,
+                  primal_space=parsed_args.primal_space,
+                    dual_space=parsed_args.dual_space,
+                    primal_degree=parsed_args.primal_degree,
+                    cell_type=parsed_args.cell_type,
+                    alpha_scheme=AlphaScheme.from_string(parsed_args.alpha_scheme),
+                    alpha_0=parsed_args.alpha_0,
+                    alpha_c=parsed_args.alpha_c,
+                    max_iterations=parsed_args.max_iterations,
+                    result_dir=parsed_args.result_dir,
+                    phi_func=phi_func,
+                    f_func=f_func,
+                    warm_start=parsed_args.warm_start)
+if __name__ == "__main__":
+    main()
+
+
+
+
