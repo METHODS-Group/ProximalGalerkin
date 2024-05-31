78472d2e631d056d9b9518420538b71d37f7055b
Jørgen S. Dokken <dokken@simula.no>
2024-05-31 15:56:45 +0200

    Find a working 3D config with partial loading

--- examples/signorini/script.py
+++ examples/signorini/script.py
@@ -1,6 +1,6 @@
 # docker run -ti -v $(pwd):/root/shared -w /root/shared jpdean/mixed_domain
 
-# SPDX-License-Identifier: LGPL-3.0-or-later
+# SPDX-License-Identifier: MIT
 
 from mpi4py import MPI
 import dolfinx
@@ -8,13 +8,6 @@ import dolfinx.fem.petsc
 import ufl
 import numpy as np
 from petsc4py import PETSc
-mesh = dolfinx.mesh.create_unit_square(MPI.COMM_WORLD, 10, 10, dolfinx.cpp.mesh.CellType.quadrilateral)
-
-def bottom_boundary(x):
-    return np.isclose(x[1], 0.0)
-
-def top_boundary(x):
-    return np.isclose(x[1], 1.0)
 
 
 class NewtonSolver():
@@ -58,42 +51,7 @@ class NewtonSolver():
         self.b.setOptionsPrefix(problem_prefix)
         self.b.setFromOptions()
 
-    # def assemble_vector(self):
-    #     """Bassed of FEniCSx block assembly"""
-    #     maps = [
-    #     (form.function_spaces[0].dofmap.index_map, form.function_spaces[0].dofmap.index_map_bs)
-    #     for form in self.F
-    # ]
-
-    #     x0_local = dolfinx.cpp.la.petsc.get_local_vectors(self.dx, maps)
-    #     x0_sub = x0_local
-
-    #     # Compute the residual without bcs and scale by -1
-    #     bcs1 = dolfinx.fem.bcs_by_block(dolfinx.fem.extract_function_spaces(self.J, 1), bcs)
-    #     with self.b.localForm() as b_local:
-    #         b_local.set(0.0)
-
-    #     b_local = dolfinx.cpp.la.petsc.get_local_vectors(self.b, maps)
-    #     for b_sub, L_sub, a_sub in zip(b_local, self.F, self.J):   
-    #         #  Compute  b - J(u_D - u_{i-1})    
-    #         dolfinx.fem.assemble_vector(b_sub, L_sub)
-    #         dolfinx.fem.apply_lifting(b_sub, a_sub, bcs1, x0_local, -1)
-
-    #     dolfinx.cpp.la.petsc.scatter_local_vectors(self.b, b_local, maps)
-    #     self.b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)          
-        
-    #     bcs0 = dolfinx.fem.bcs_by_block(dolfinx.fem.extract_function_spaces(self.F), bcs)
-    #     offset = 0
-    #     b_array = self.b.getArray(readonly=False)
-    #     print(b_array)
-    #     for submap, bc, _x0 in zip(maps, bcs0, x0_sub):
-    #         size = submap[0].size_local * submap[1]
-    #         dolfinx.fem.set_bc(b_array[offset : offset + size], bc, _x0, -1)
-    #         offset += size
-    #         print(_x0)
-
-    #     self.b.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-
+   
     def solve(self, tol=1e-6, beta=1.0):
         i = 0
 
@@ -109,7 +67,6 @@ class NewtonSolver():
             self.x.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
 
             # Assemble F(u_{i-1}) - J(u_D - u_{i-1}) and set du|_bc= u_D - u_{i-1}
-            #self.assemble_vector()
             self.b.zeroEntries()
             dolfinx.fem.petsc.assemble_vector_block(self.b, self.F,self.J, bcs=self.bcs,x0=self.x, scale=-1.0)
             self.b.ghostUpdate(PETSc.InsertMode.INSERT_VALUES, PETSc.ScatterMode.FORWARD)
@@ -128,14 +85,24 @@ class NewtonSolver():
                 s.x.array[:num_sub_dofs] -= beta*self.dx.array_r[offset_start:offset_start+num_sub_dofs]
                 s.x.scatter_forward()
                 offset_start += num_sub_dofs
-                print(s.x.array, self.b.array_r[offset_start:offset_start+num_sub_dofs])
             # Compute norm of update
+
             correction_norm = self.dx.norm(0)
             print(f"Iteration {i}: Correction norm {correction_norm}")
             if correction_norm < tol:
                 break
             i += 1
 
+
+
+def bottom_boundary(x):
+    return np.isclose(x[2], 0.0)
+
+def top_boundary(x):
+    return np.isclose(x[2], 1.0)
+
+
+mesh = dolfinx.mesh.create_unit_cube(MPI.COMM_WORLD, 10, 10, 10, dolfinx.cpp.mesh.CellType.tetrahedron)
 degree = 1
 gdim = mesh.geometry.dim
 fdim = mesh.topology.dim - 1
@@ -150,7 +117,7 @@ mt = dolfinx.mesh.meshtags(mesh, mesh.topology.dim-1, contact_facets, np.full_li
 submesh, submesh_to_mesh = dolfinx.mesh.create_submesh(mesh, mesh.topology.dim-1, contact_facets)[0:2]
 
 
-E, nu = 2.0e2, 0.3
+E, nu = 2.0e4, 0.3
 mu = E / (2.0 * (1.0 + nu))
 lmbda = E * nu / ((1.0 + nu) * (1.0 - 2.0 * nu))
 
@@ -160,11 +127,23 @@ def epsilon(w):
 def sigma(w, gdim):
     return 2.0 * mu * epsilon(w) + lmbda * ufl.tr(ufl.grad(w)) * ufl.Identity(gdim)
 
+import basix.ufl
+enriched_element = basix.ufl.enriched_element(
+    [basix.ufl.element("Lagrange", mesh.topology.cell_name(), degree),
+     basix.ufl.element("Bubble", mesh.topology.cell_name(), degree+mesh.geometry.dim)])
+# FIXME: We need facet bubbles
+#element_u = basix.ufl.blocked_element(enriched_element, shape=(mesh.geometry.dim, ))
+#element_p = basix.ufl.element("Lagrange", submesh.topology.cell_name(), degree-1, discontinuous=True)
 
-V = dolfinx.fem.functionspace(mesh, ("Lagrange", degree, (mesh.geometry.dim, )))
+element_u = basix.ufl.element("Lagrange", mesh.topology.cell_name(), degree, shape=(mesh.geometry.dim, ))
+V = dolfinx.fem.functionspace(mesh, element_u)
+
 u = dolfinx.fem.Function(V)
 v = ufl.TestFunction(V)
-W = dolfinx.fem.functionspace(submesh, ("Lagrange", degree))
+
+
+element_p = basix.ufl.element("Lagrange", submesh.topology.cell_name(), degree)
+W = dolfinx.fem.functionspace(submesh, element_p)
 
 v = ufl.TestFunction(V)
 psi = dolfinx.fem.Function(W)
@@ -179,9 +158,10 @@ entity_maps = {submesh: mesh_to_submesh}
 ds = ufl.Measure("ds", domain=mesh, subdomain_data=mt, subdomain_id=1)
 n = ufl.FacetNormal(mesh)
 alpha = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type(1.0))
-f = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type((0.0, 0.0)))
+f = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type((0.0, 0.0, -0.0)))
+x = ufl.SpatialCoordinate(mesh)
 g = dolfinx.fem.Constant(submesh, dolfinx.default_scalar_type(0.0))
-
+#g = ufl.conditional(ufl.lt((x[0]-0.5)**2+(x[1]-0.5)&&, 0.9), 0, 1)
 
 F00 = alpha * ufl.inner(sigma(u, mesh.geometry.dim), ufl.grad(v)) * ufl.dx(domain=mesh) - alpha * ufl.inner(f, v) * ufl.dx(domain=mesh)
 F01 = -ufl.inner(psi-psi_k, ufl.dot(v, n)) * ds
@@ -207,14 +187,26 @@ J = [[J00, J01], [J10, J11]]
 F = [residual_0, residual_1]
 
 u_bc = dolfinx.fem.Function(V)
-u_bc.interpolate(lambda x: (np.full(x.shape[1], 0.0), np.full(x.shape[1], -0.05)))
+disp = -0.1
+u_bc.interpolate(lambda x: (np.full(x.shape[1], 0.0), np.full(x.shape[1], 0.0), np.full(x.shape[1], disp)))
+V0, V0_to_V = V.sub(2).collapse()
 bc = dolfinx.fem.dirichletbc(u_bc, dolfinx.fem.locate_dofs_topological(V, fdim, top_facets))
 bcs = [bc]
 
 
-solver = NewtonSolver(F, J, [u, psi], bcs=bcs, max_iterations=5)
+solver = NewtonSolver(F, J, [u, psi], bcs=bcs, max_iterations=25, petsc_options={"ksp_type":"preonly", "pc_type":"lu", 
+"pc_factor_mat_solver_type":"mumps"})
 
 bp = dolfinx.io.VTXWriter(mesh.comm, "uh.bp", [u], engine="BP4")
-solver.solve(1e-6, 1)
-bp.write(0.0)
+bp_psi = dolfinx.io.VTXWriter(mesh.comm, "psi.bp", [psi], engine="BP4")
+
+M = 10
+for it in range(M):
+    u_bc.x.array[V0_to_V] = (it+1)/M * disp
+    print((it+1)/M * disp)
+    #alpha.value += 0.
+    solver.solve(1e-4, 1)
+    psi_k.x.array[:] = psi.x.array
+    bp.write(it)
+    bp_psi.write(it)
 bp.close()
